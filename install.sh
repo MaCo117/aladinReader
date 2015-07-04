49f8451fe08cab58082fde7b5d9986f7720aa27a
MaCo117 <marcel.kebisek@gmail.com>
2015-07-04 17:54:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 
+# AladinReader - SHMU Aladin forecast data extractor
+# Copyright (C) 2015 Marcel Kebisek
+# Contact: marcel.kebisek@gmail.com
+#
+# This file is part of AladinReader.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# NOTE: 
+# All forecast data are property of Slovak hydrometeorogical institute (SHMU)
+# and should be used under approval of SHMU. Author takes ABSOLUTELY no
+# responsibility for improper use of this program.
+
+
 RCol='\e[0m'
 Red='\e[0;31m'
 Gre='\e[0;32m'
